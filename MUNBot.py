d588051ffc062d4413342d3cd045f4b6793447a5
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-05-17 15:41:39 +0530

    Added attendance

--- MUNBot.py
+++ MUNBot.py
@@ -4,7 +4,7 @@ import random
 #import xlsx
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '#')
+client = commands.Bot(command_prefix = '')
 
 @client.event
 async def on_ready():
@@ -17,9 +17,11 @@ async def on_ready():
 async def noice(ctx):
 	await ctx.send(f'Noice.\n{round(client.latency*1000)}ms')
 
+@client.command()    
+async def Attendence(ctx, name, year, stream):
+	await ctx.send(f'{name}, noted')
 
 
 
 
-
-client.run('ODQzNzg2MDYyMTQ0ODY0MjY2.YKI7Ag.fxIZrjAexauw_TEaN7Y-lkG-JR4')
+client.run('token')
