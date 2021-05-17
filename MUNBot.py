5a548cc00692f754615413ebb93a527c93a6fa81
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-05-17 23:45:25 +0530

    removed extra code

--- MUNBot.py
+++ MUNBot.py
@@ -5,10 +5,10 @@ import xlrd
 import env
 from xlwt import Workbook
 from discord.ext import commands
-import csv #csv module imported
+import csv
 
 
-#token change required
+TOKEN='ODQzNzg2MDYyMTQ0ODY0MjY2.YKI7Ag.EA0pBFPgYyVlxBM0hwAuF1NTX4Q'
 CHANNEL_ID=int(763390600103854114) #channel_id change required
 VOICE_ID=int(843865682760302602) #voice_id change required
 client = commands.Bot(command_prefix = '')
@@ -16,16 +16,6 @@ client = commands.Bot(command_prefix = '')
 @client.event
 async def on_ready():
 	print('Bot is Ready.')
-
-'''
-# Workbook is created
-wb = Workbook()
-
-# add_sheet is used to create sheet.
-sheet1 = wb.add_sheet('Sheet 1')
-
-#workbook = xlrd.open_workbook('data.xlsx')
-'''
 
 #FUNCTIONS
 @client.command()            
@@ -38,8 +28,6 @@ async def noice(ctx):
 
 @client.command()    
 async def Attendence(ctx, name, year, stream):
-	#sheet1.write(name, year, stream)
-	#wb.save('data.xlsx')
 
 	#New Code From Here
 	print(name,year,stream)
