8c5cd6c3124b18805106869632a850b302e240c2
SuryaSekhar14 <suryasekhardatta22@gmail.com>
2021-05-18 17:55:16 +0530

    Added vc auth to attendance

--- MUNBot.py
+++ MUNBot.py
@@ -1,15 +1,13 @@
 import discord
 import time
 from discord.member import VoiceState
-import xlrd
-from xlwt import Workbook
 from discord.ext import commands
 import csv
 
 
-TOKEN = ''
-CHANNEL_ID=int(753472470354362476)
-VOICE_ID=int(753472470773923861)
+TOKEN = 'ODQzNzg2MDYyMTQ0ODY0MjY2.YKI7Ag.SH3036PIIBbjfHrWiJ0nTGslHfs'
+CHANNEL_ID=int(763390600103854114)
+VOICE_ID=int(843865682760302602)
 client = commands.Bot(command_prefix = '')
 
 @client.event
@@ -24,25 +22,31 @@ async def on_ready():
 async def ping(ctx):
 	await ctx.send(f'What do you expect, Pong?\nLatency= {round(client.latency*1000)}ms')
 	print(ctx.author)
-	print(ctx.author.id)
-
 
 @client.command()    
 async def Attendance(ctx, *nys):
-
-	#New Code From Here
-	inter=list(nys)
-	info=inter
-	n=len(info)
-	with open('attendees.csv','a',newline='') as file:
-		fieldnames=["Name","Stream","Year"]
-		writer=csv.DictWriter(file,fieldnames=fieldnames)
-		if(n==4):
-			writer.writerow({"Name":info[0]+info[1],"Stream":info[2],"Year":info[3]}) 
-		if(n==5):
-			writer.writerow({"Name":info[0]+' '+info[1]+' '+info[2],"Stream":info[3],"Year":info[4]})
-	await ctx.send(f'{info[0]}, noted')
-
+	vcchannel=client.get_channel(VOICE_ID)
+	try:
+		#############
+		user = ctx.message.author
+		vc = user.voice.channel
+		if (vc == vcchannel):
+			inter=list(nys)
+			info=inter
+			n=len(info)
+			with open('attendees.csv','a',newline='') as file:
+				fieldnames=["Name","Stream","Year"]
+				writer=csv.DictWriter(file,fieldnames=fieldnames)
+				if(n==4):
+					writer.writerow({"Name":info[0]+' '+info[1],"Stream":info[2],"Year":info[3]}) 
+				if(n==5):
+					writer.writerow({"Name":info[0]+' '+info[1]+' '+info[2],"Stream":info[3],"Year":info[4]})
+			#############
+			await ctx.send(f'**{ctx.author}**, Attendance Recorded')
+		else:
+			await ctx.send(f'**{ctx.author}**, you are in another Voice Channel')
+	except:
+		await ctx.send(f'**{ctx.author}**, You must be in a Voice Channel')
 
 @client.command(pass_context=True)    
 async def join(ctx):
@@ -52,8 +56,11 @@ async def join(ctx):
 
 @client.command(pass_context=True)    
 async def leave(ctx):
-	await ctx.guild.voice_client.disconnect()
-	await ctx.send("Left!")
+	try:
+		await ctx.guild.voice_client.disconnect()
+		await ctx.send("Left the Meeting")
+	except:
+		await ctx.send("I am not in the Meeting.")
 
 @client.command(pass_context=True)
 async def att(ctx):
@@ -63,8 +70,9 @@ async def att(ctx):
 		vc = user.voice.channel
 		if (vc == vcchannel):
 			await ctx.send(f'**{ctx.author}**, Attendance Recorded')
+		else:
+			await ctx.send(f'**{ctx.author}**, you are in another Voice Channel')
 	except:
-		await ctx.send(f'**{ctx.author}**, You must be in the Meeting')
-
+		await ctx.send(f'**{ctx.author}**, You must be in a Voice Channel')
 
 client.run(TOKEN)
